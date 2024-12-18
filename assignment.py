90bed974687feeb0930747e76b816be0dde2eb86
aliensbright <144503232+aliensbright@users.noreply.github.com>
2024-12-18 11:43:21 -0800

    ?

--- assignment.py
+++ assignment.py
@@ -30,10 +30,18 @@ You will be using a while or for loop to generate multiple data entries to store
 
 world = {'starport':'','navalBase':'','scoutBase':'','gasGiant':'','name':'','size':'','atmosphere':'','hydrographics':'','population':'','government level':'','law level':'','tech level':'',}
 
-starport  = ['A'  ,'A'  ,'A'  ,'B'  ,'B'  ,'C'  ,'C'  ,'D'  ,'E ' ,'E ' ,'X']
-navalBase = ['no' ,'no' ,'no' ,'no' ,'no' ,'no' ,'yes','yes','yes','yes','yes']
-scoutBase = ['no' ,'no' ,'no' ,'no' ,'no' ,'yes','yes','yes','yes','yes','yes']
-gasGiant  = ['yes','yes','yes','yes','yes','yes','yes','yes','no' ,'no' ,'no']
+starport  = ('A'  ,'A'  ,'A'  ,'B'  ,'B'  ,'C'  ,'C'  ,'D'  ,'E ' ,'E ' ,'X')
+navalBase = ('no' ,'no' ,'no' ,'no' ,'no' ,'no' ,'yes','yes','yes','yes','yes')
+scoutBase = ('no' ,'no' ,'no' ,'no' ,'no' ,'yes','yes','yes','yes','yes','yes')
+gasGiant  = ('yes','yes','yes','yes','yes','yes','yes','yes','no' ,'no' ,'no')
+size = ('Asteroid/Planetoid','Ring (around a world)','Small world (200 km)','1,600 km','3,200 km','4,800 km','6,400 km','8,000 km','9,600 km','11,200 km','12,800 km','14,400 km','16,000 km')
+atmosphere = ('None.','Trace.','Very thin, tainted.','Very thin.','Thin, tainted.','Thin.','Standard.','Standard, tainted.','Dense.','Dense, tainted.','Exotic.','Corrosive.','Insidious.','Dense, high.','Elipsoid.','Thin, low.')
+hydrographics = ('No free standing Water.','10% Water','20% Water','30% Water','40% Water','50% Water','60% Water','70% Water','80% Water','90% Water','No Land Masses.',)
+population = ('None.','10s','100s','1,000s','10,000s','100,000s','1,000,000s','10,000,000s','100,000,000s','1,000,000,000s','10,000,000,000s')
+governmentLevel = ('No Government Structure.','Company/Corporation','Participating Democracy.','Self-Perpetuating Oligarchy.','Representative Democracy.','Feudal Technocracy.','Captive Government.','Balkanization.','Civil Service Bureaucracy.','Impersonal Bureaucracy.','Charismatic Dictator.','Non-Charismatic Leader.','Charismatic Oligarchy.','Religious Dictatorship.')
+lawLevel = ('No Prohibitions.','Body Pistols, Explosives, and Poison Gas Prohibited.','Portable Energy Weapons Prohibited.','Military Weapons Prohibited.','Light Assault Weapons Prohibited.','Concealable Firearms Prohibited.','Most Firearms Prohibited.','Shotguns Prohibited.','Open Possession of Long Bladed Weapons Prohibited.','Open Possession of any Weapon Prohibited.','Weapon Possession Prohibited.','','','','','','','','')
+techLevel = ('Stone Age. Primitive.','Bronze Age to Middle Ages.','Circa 1400 to 1700','Circa 1700 to 1860','Circa 1860 to 1900','Circa 1900 to 1939','Circa 1940 to 1969','Circa 1970 to 1979','Circa 1980 to 1989','Circa 1990 to 2000','Interstellar community','Average Imperial.','Average Imperial.','Above Average Imperial.','Above Average Imperial.','Technical Maximum Imperial.','Occasional non-Imperial.')
+
 values = (starport,navalBase,scoutBase,gasGiant)
 valuesDict = ('starport','navalBase','scoutBase','gasGiant')
 namestring = 'POIUYTREWQASDFGHJKLMNBVCXZ0987654321'
@@ -45,6 +53,8 @@ def getsystemcontents():
         roll1 = random.randint(1,6)
         roll2 = random.randint(1,6)
         totalindex = roll1 + roll2 - 2
+        if world['starport']=='C':
+            
         world[dictThing] = val[totalindex]
 
 def generateName():
@@ -55,9 +65,19 @@ def generateName():
         name += stringval
     return name
 
-worldName = generateName()
+def generateSize():
 
-print(world)
+def generateAtmosphere():
+
+    
+
+
+world['name'] = generateName()
+getsystemcontents()
+
+
+for i in world:
+    print(i,world[i])
 
 
 
