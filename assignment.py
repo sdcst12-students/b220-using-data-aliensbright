61231c729634fb3ce47cc922f0406e6a8c30d25d
aliensbright <144503232+aliensbright@users.noreply.github.com>
2025-01-02 20:04:45 -0800

    Dont know modifiers for hydro

--- assignment.py
+++ assignment.py
@@ -29,18 +29,30 @@ You will be using a while or for loop to generate multiple data entries to store
 """
 
 world = {'starport':'','navalBase':'','scoutBase':'','gasGiant':'','name':'','size':'','atmosphere':'','hydrographics':'','population':'','government level':'','law level':'','tech level':'',}
-
+'''
 starport  = ('A'  ,'A'  ,'A'  ,'B'  ,'B'  ,'C'  ,'C'  ,'D'  ,'E ' ,'E ' ,'X')
 navalBase = ('no' ,'no' ,'no' ,'no' ,'no' ,'no' ,'yes','yes','yes','yes','yes')
 scoutBase = ('no' ,'no' ,'no' ,'no' ,'no' ,'yes','yes','yes','yes','yes','yes')
 gasGiant  = ('yes','yes','yes','yes','yes','yes','yes','yes','no' ,'no' ,'no')
-size = ('Asteroid/Planetoid','Ring (around a world)','Small world (200 km)','1,600 km','3,200 km','4,800 km','6,400 km','8,000 km','9,600 km','11,200 km','12,800 km','14,400 km','16,000 km')
+size = ('Asteroid/Planetoid','1,600 km','3,200 km','4,800 km','6,400 km','8,000 km','9,600 km','11,200 km','12,800 km','14,400 km','16,000 km')
 atmosphere = ('None.','Trace.','Very thin, tainted.','Very thin.','Thin, tainted.','Thin.','Standard.','Standard, tainted.','Dense.','Dense, tainted.','Exotic.','Corrosive.','Insidious.','Dense, high.','Elipsoid.','Thin, low.')
 hydrographics = ('No free standing Water.','10% Water','20% Water','30% Water','40% Water','50% Water','60% Water','70% Water','80% Water','90% Water','No Land Masses.',)
 population = ('None.','10s','100s','1,000s','10,000s','100,000s','1,000,000s','10,000,000s','100,000,000s','1,000,000,000s','10,000,000,000s')
-governmentLevel = ('No Government Structure.','Company/Corporation','Participating Democracy.','Self-Perpetuating Oligarchy.','Representative Democracy.','Feudal Technocracy.','Captive Government.','Balkanization.','Civil Service Bureaucracy.','Impersonal Bureaucracy.','Charismatic Dictator.','Non-Charismatic Leader.','Charismatic Oligarchy.','Religious Dictatorship.')
+governmentLevel = ('No Government Structure.','Company/Corporation.','Participating Democracy.','Self-Perpetuating Oligarchy.','Representative Democracy.','Feudal Technocracy.','Captive Government.','Balkanization.','Civil Service Bureaucracy.','Impersonal Bureaucracy.','Charismatic Dictator.','Non-Charismatic Leader.','Charismatic Oligarchy.','Religious Dictatorship.')
 lawLevel = ('No Prohibitions.','Body Pistols, Explosives, and Poison Gas Prohibited.','Portable Energy Weapons Prohibited.','Military Weapons Prohibited.','Light Assault Weapons Prohibited.','Concealable Firearms Prohibited.','Most Firearms Prohibited.','Shotguns Prohibited.','Open Possession of Long Bladed Weapons Prohibited.','Open Possession of any Weapon Prohibited.','Weapon Possession Prohibited.','','','','','','','','')
 techLevel = ('Stone Age. Primitive.','Bronze Age to Middle Ages.','Circa 1400 to 1700','Circa 1700 to 1860','Circa 1860 to 1900','Circa 1900 to 1939','Circa 1940 to 1969','Circa 1970 to 1979','Circa 1980 to 1989','Circa 1990 to 2000','Interstellar community','Average Imperial.','Average Imperial.','Above Average Imperial.','Above Average Imperial.','Technical Maximum Imperial.','Occasional non-Imperial.')
+'''
+starport  = ('A'  ,'A'  ,'A'  ,'B'  ,'B'  ,'C'  ,'C'  ,'D'  ,'E ' ,'E ' ,'X')
+navalBase = ('no' ,'no' ,'no' ,'no' ,'no' ,'no' ,'yes','yes','yes','yes','yes')
+scoutBase = ('no' ,'no' ,'no' ,'no' ,'no' ,'yes','yes','yes','yes','yes','yes')
+gasGiant  = ('yes','yes','yes','yes','yes','yes','yes','yes','no' ,'no' ,'no')
+size = (0,1,2,3,4,5,6,7,8,9,'A')
+atmosphere = (0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F')
+hydrographics = (0,1,2,3,4,5,6,7,8,9,'A')
+population = (0,1,2,3,4,5,6,7,8,9,'A')
+governmentLevel = (0,1,2,3,4,5,6,7,8,9,'A+')
+lawLevel = (0,1,2,3,4,5,6,7,8,9,'A','B','C','D')
+techLevel = (0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G')
 
 values = (starport,navalBase,scoutBase,gasGiant)
 valuesDict = ('starport','navalBase','scoutBase','gasGiant')
@@ -52,9 +64,7 @@ def getsystemcontents():
         dictThing = valuesDict[i]
         roll1 = random.randint(1,6)
         roll2 = random.randint(1,6)
-        totalindex = roll1 + roll2 - 2
-        if world['starport']=='C':
-            
+        totalindex = roll1 + roll2 - 2  #-2 so that is goes to the right index value
         world[dictThing] = val[totalindex]
 
 def generateName():
@@ -66,18 +76,101 @@ def generateName():
     return name
 
 def generateSize():
+    roll1 = random.randint(1,6)
+    roll2 = random.randint(1,6)
+    totalindex = roll1 + roll2 - 2
+    world['size']=size[totalindex]
+    return totalindex
 
-def generateAtmosphere():
-
+def generateAtmosphere(sIndex):
+    if sIndex==0:
+        world['atmosphere']=atmosphere[0]
+        return 0
+    else:
+        roll1 = random.randint(1,6)
+        roll2 = random.randint(1,6)
+        totalindex = roll1 + roll2 - 7 + sIndex
+        world['atmosphere'] = atmosphere[totalindex]
+        return totalindex
     
+
+def generatePopulation():
+    roll1 = random.randint(1,6)
+    roll2 = random.randint(1,6)
+    totalindex = roll1 + roll2 - 2
+    world['population'] = population[totalindex]
+    return totalindex
+
+def generateHydrographics(sIndex,aIndex):
+    roll1 = random.randint(1,6)
+    roll2 = random.randint(1,6)
+    totalindex = roll1 + roll2 - 7 + sIndex 
+    world['hydrographics'] = hydrographics[totalindex] #Sill don't know what the dm are
+    
+
+def generateGovernment(pIndex):
+    roll1 = random.randint(1,6)
+    roll2 = random.randint(1,6)
+    totalindex = roll1 + roll2 - 7 + pIndex 
+    world['government level'] = governmentLevel[totalindex]
+    return totalindex
+
+def generateLaw(gIndex):
+    roll1 = random.randint(1,6)
+    roll2 = random.randint(1,6)
+    totalindex = roll1 + roll2 - 7 + gIndex 
+    world['law level']= lawLevel[totalindex]
+    return totalindex
+
+def generateTech():
+    totalindex = random.randint(1,6)
+    if world['starport']=='A':
+        totalindex += 6
+    if world['starport']=='B':
+        totalindex += 4
+    if world['starport']=='C':
+        totalindex += 2
+    if world['starport']=='X':
+        totalindex -= 4
+    if world['size'] <= 1:
+        totalindex += 2
+    if 1 < world['size'] <= 4:
+        totalindex += 1
+    if world['atmosphere'] <= 3 or 10 <= atindex <= 14:
+        totalindex += 1
+    if world['hydrographics'] == 9:
+        totalindex += 1
+    if world['hydrographics'] == 'A':
+        totalindex += 2
+    if 1 <= world['population'] <= 5:
+        totalindex += 1
+    if world['population'] == 9:
+        totalindex += 2
+    if world['population'] == 'A':
+        totalindex += 4
+    if world['government level'] == 0 or world['government level'] == 5:
+        totalindex += 1
+    if world['government level'] == 'D':
+        totalindex -= 2
+    world['tech level'] = techLevel[totalindex]
 
 
 world['name'] = generateName()
 getsystemcontents()
+starportType = world['starport'] 
+sizeindex = generateSize()
+atindex = generateAtmosphere(sizeindex)
+popindex = generatePopulation()
+generateHydrographics(sizeindex,atindex)
+govindex = generateGovernment(popindex)
+generateLaw(govindex)
+generateTech()
+
+
 
 
 for i in world:
-    print(i,world[i])
-
+    print(' ',i,':',world[i])
+print('\n\n',world)
 
 
